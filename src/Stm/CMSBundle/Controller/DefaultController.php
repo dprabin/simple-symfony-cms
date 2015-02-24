19c8417600f8125a45509060b04e0795c82821b0
dprabin <dprabin@yahoo.com>
2015-02-25 00:43:02 +0545

    added displayAction() method to handle displaying of pages with

--- src/Stm/CMSBundle/Controller/DefaultController.php
+++ src/Stm/CMSBundle/Controller/DefaultController.php
@@ -10,8 +10,12 @@ class DefaultController extends Controller
     {
     	$em = $this->getDoctrine()->getManager();
     	$pages = $em->getRepository('StmCMSBundle:Page')->findAll();
-
-
         return $this->render('StmCMSBundle:Default:index.html.twig', array('pages'=>$pages));
+    }
+
+    public function displayAction($id){
+    	$em = $this->getDoctrine()->getManager();
+    	$page = $em->getRepository('StmCMSBundle:Page')->find($id);
+        return $this->render('StmCMSBundle:Default:display.html.twig', array('page'=>$page));
     }
 }
