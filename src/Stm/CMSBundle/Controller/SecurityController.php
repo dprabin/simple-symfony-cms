884aabae58ce607e7b6d8d64f06bf38378d53646
dprabin <dprabin@yahoo.com>
2015-02-24 08:57:53 +0545

    Symfony2.6+ version support for security authentication

--- src/Stm/CMSBundle/Controller/SecurityController.php
+++ src/Stm/CMSBundle/Controller/SecurityController.php
@@ -10,6 +10,9 @@ class SecurityController extends Controller
 {
 	public function loginAction(Request $request)
 	{
+		/*** this is for compatibility with symfony earlier version
+		* symfony 2.6 and above support much sleek method to authenticate
+
 		$session = $request->getSession();
 
 		//Get the login error if there is any
@@ -24,10 +27,19 @@ class SecurityController extends Controller
 
 		//Last username entered by the user
 		$lastUsername = (null === $session) ? '' : $session->get(SecurityContextInterface::LAST_USERNAME);
+		*/
+		
+		$authenticationUtils = $this->get('security.authentication_utils');
 
+		//get the login error if any
+		$error = $authenticationUtils->getLastAuthenticationError();
+		//Last username entered by the user
+		$lastUsername = $authenticationUtils->getLastUsername
+
+		//render the view
 		return $this->render(
 			'StmCMSBundle:Security:login.html.twig', 
-			array('error' => $error,)
+			array('error' => $error,'last_username' => $lastUsername,)
 		);
 	}
 }
