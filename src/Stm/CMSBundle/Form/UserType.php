b495952528d3d50f604ff9e43b664864ce7b4911
dprabin <dprabin@yahoo.com>
2015-02-24 06:06:55 +0545

    generate crud for User entity with generate:doctrine:crud and route to /cms/user

---
+++ src/Stm/CMSBundle/Form/UserType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Stm\CMSBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('password')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Stm\CMSBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'stm_cmsbundle_user';
+    }
+}
